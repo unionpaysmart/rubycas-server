aaed13524425ac95e9f0d0ea6908f109523afe57
Martin Xu <martin.xus@gmail.com>
2013-12-17 17:58:47 +0800

    enable mobile and email login

--- lib/casserver/authenticators/sql_devise.rb
+++ lib/casserver/authenticators/sql_devise.rb
@@ -32,17 +32,35 @@ require 'bcrypt/password'
 class CASServer::Authenticators::SQLDevise < CASServer::Authenticators::SQL
 
   def validate(credentials)
+    email_name_regex  = '[\w\.%\+\-]+'.freeze
+    domain_head_regex = '(?:[A-Z0-9\-]+\.)+'.freeze
+    domain_tld_regex  = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
+    email_regex       = /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
+
     read_standard_credentials(credentials)
     raise_if_not_configured
 
     user_model = self.class.user_model
 
     username_column = @options[:username_column] || "login"
+    email_column = @options[:email_column]
+    mobile_column = @options[:mobile_column]
     password_column = @options[:password_column] || "crypted_password"
     pepper   = @options[:pepper]||''
 
     log_connection_pool_size
-    results = user_model.find(:all, :conditions => ["#{username_column} = ?", @username])
+
+    if  @username =~ /^\d{11}$/ && mobile_column.present?
+      $LOG.info "Login with mobile:#{@username}"
+      results = user_model.find(:all, :conditions => ["#{mobile_column} = ?", @username])
+    elsif @username =~ email_regex && email_column.present?
+      $LOG.info "Login with email:#{@username}"
+      results = user_model.find(:all, :conditions => ["#{email_column} = ?", @username])
+    else
+      $LOG.info "Login with username:#{@username}"
+      results = user_model.find(:all, :conditions => ["#{username_column} = ?", @username])
+    end
+
     user_model.connection_pool.checkin(user_model.connection)
 
     if results.size > 0
